35f1a547dff36b4a69bc330b0ea9186f17e151c4
LocutusV0nB0rg <leon@familie-breidt.de>
2021-02-24 13:18:09 +0100

    Initial commit

---
+++ src/main/java/borg/locutus/moneyshot/MoneyShot.java
@@ -0,0 +1,23 @@
+package borg.locutus.moneyshot;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+import java.util.List;
+
+public class MoneyShot extends LabyModAddon {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+}
